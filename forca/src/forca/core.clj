8d5a55b0aaa1012e2bf70edf38c0b409d63771ad
Márcio Alexandre Freire Sindeaux <sindeaux1999@hotmail.com>
2019-08-15 21:20:51 -0300

    feat: Ajuste de novos métodos para o jogo

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -3,16 +3,31 @@
 
 (def total-de-vidas 6)
 
-(defn perdeu [] (print "\nVoce Perdeu\n"))
+(defn perdeu [] (print "\nVoce Perdeu\n") )
+(defn ganhou [] (print "\nVoce Ganhou\n") )
 
-(defn jogo [vidas]
+
+(defn conferencia-letras [palavras acerto]
+  (remove (
+      fn [letra] 
+        (contains? acerto (str letras))
+    ) 
+    palavras
+  )
+)
+
+(defn acertou-palavra-toda? [palavra acertos] 
+  (empty? (conferencia-letras palavra acerto))
+)
+
+
+(defn jogo [vidas palavra acertos]
   (if (= vidas 0) 
-    (perdeu)
-    (do
-      (print vidas)
-      (jogo (- vidas 1))
+      (perdeu)
+    (if (acertou-palavra-toda? palavra acertos)
+      (ganhou)
+      (print "Chuta amigo !")
     )
-    
   )
 )
 
